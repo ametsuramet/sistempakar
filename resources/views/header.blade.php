3225b5c6961da9c0fbc631ecc704b63233f29ca7
ametsuramet <ametsuramet@gmail.com>
2015-07-13 12:53:37 +0700

    edit dashboard

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="index.html">Sistem Pakar</a>
+    <a class="navbar-brand" href="index.html">Kepakaran Peneliti</a>
 </div>
             <!-- /.navbar-header -->
             @if(Auth::user())
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,15 @@
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Sistem Pakar</h1>
+                    <h1 class="page-header">Pusat Penelitian Geoteknologi LIPI</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
             <div class="row">
                 <div class="col-lg-12 col-md-6">
-                    <p>Selamat Datang di Sistem Informasi Kepakaran.</p>
+                    <p>Selamat Datang di Sistem Informasi Kepakaran Pusat Penelitian Geoteknologi LIPI, sistem ini memudahkan anda untuk mengakses basis data peneliti di lingkungan Pusat Penelitian Geoteknologi LIPI.</p>
+                    <p>Untuk memudahkan pencarian data peneliti, silakan gunakan fasilitas Search di pojok kanan atas.</p>
                 </div>
             </div>
             <!-- /.row -->
