3e72359c5273ec11fa338da70a69ca5f8a8abaef
ametsuramet <ametsuramet@gmail.com>
2015-07-25 20:48:34 +0700

    tambah logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Pusat Penelitian Geoteknologi LIPI</h1>
+                    <h1 class="page-header"><img src="/images/logo.jpg" height="100" /> Pusat Penelitian Geoteknologi LIPI</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
